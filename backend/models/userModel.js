eec5a0bacc5f9e4548a7f187f4aa0d36ea4887f3
joneirikboska <joneirikboska@gmail.com>
2020-10-11 11:53:18 +0200

    Create actions, constants and reducers for user login.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,21 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+// middleware for hashing password on create user
+userSchema.pre('save', async function(next) {
+  /*
+  check to see if password is sent or modified
+  this way, password won't be re-hashed on profile update where
+  only unrelated fields are modified
+  */
+  if(!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
